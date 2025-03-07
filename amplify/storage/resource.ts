40d7eb4b0a88190c1d3d837a6dc6613c77cf99b7
IdhcbIan <idhcb.ian@gmail.com>
2025-03-07 19:35:17 -0300

    At 5

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,15 +1,16 @@
 import { defineStorage } from '@aws-amplify/backend';
+import { allow } from '@aws-amplify/backend-storage';
 
 export const storage = defineStorage({
   name: 'myAppStorage',
-  access: (allow) => ({
+  access: () => ({
     // Public read access for all files in the "public" folder
-    public: allow.guest().read('public/*'),
+    public: allow.guest().to.read('public/*'),
     
     // Authenticated users can read/write their own files
-    authenticated: allow.authenticated().readWrite('${user}/*'),
+    authenticated: allow.authenticated().to.readWrite('${user}/*'),
     
     // Admin users can access all files
-    admin: allow.groups(['admin']).readWriteDelete('*'),
+    admin: allow.groups(['admin']).to.readWrite('*'),
   }),
 });
